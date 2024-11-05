93e2e1be67183f2255f0b9b0c59ec46764d9e8dc
danielpatricio0022 <hadukens@hotmail.com>
2024-11-05 19:34:17 -0300

    hi

---
+++ src/main/java/org/api/model/Event.java
@@ -0,0 +1,10 @@
+package org.api.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "evet")
+@Entity
+public class Event {
+
+
+}
---
+++ src/main/java/org/api/model/Participants.java
@@ -0,0 +1,8 @@
+package org.api.model;
+
+public class Participants {
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+}
