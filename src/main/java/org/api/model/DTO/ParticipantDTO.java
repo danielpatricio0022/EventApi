d2bef1159ac269a7370aaa9dd45499bc40d30b67
DANIEL LUCAS PATRICIO DE CARVALHO (EXTERNAL) <SE66162@fiatauto.adfa.local>
2024-11-06 10:25:47 -0300

    added dto

---
+++ src/main/java/org/api/model/DTO/ParticipantDTO.java
@@ -0,0 +1,41 @@
+package org.api.model.DTO;
+
+public class ParticipantDTO {
+
+  private Long participanteId;
+  private String nome;
+  private String email;
+  private boolean status;
+
+  public Long getParticipanteId() {
+    return participanteId;
+  }
+
+  public void setParticipanteId(Long participanteId) {
+    this.participanteId = participanteId;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public boolean isStatus() {
+    return status;
+  }
+
+  public void setStatus(boolean status) {
+    this.status = status;
+  }
+}
---
+++ src/main/java/org/api/model/DTO/ResponseEventParticipants.java
@@ -0,0 +1,36 @@
+package org.api.model.DTO;
+
+import java.util.List;
+
+public class ResponseEventParticipants {
+
+  private Long eventoId;
+  private String nomeEvento;
+  private List<ParticipantDTO> participantes;
+
+
+
+  public Long getEventoId() {
+    return eventoId;
+  }
+
+  public void setEventoId(Long eventoId) {
+    this.eventoId = eventoId;
+  }
+
+  public String getNomeEvento() {
+    return nomeEvento;
+  }
+
+  public void setNomeEvento(String nomeEvento) {
+    this.nomeEvento = nomeEvento;
+  }
+
+  public List<ParticipantDTO> getParticipantes() {
+    return participantes;
+  }
+
+  public void setParticipantes(List<ParticipantDTO> participantes) {
+    this.participantes = participantes;
+  }
+}
